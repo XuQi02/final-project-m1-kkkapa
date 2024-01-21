31259791d31a43d56af14c9960f215ea7f5d4bc8
Rhit-xuq2 <95315761+XuQi02@users.noreply.github.com>
2024-01-21 15:05:34 -0500

    change

--- MultifileTemplate/QiXumutifile/motorfunction.ino
+++ MultifileTemplate/QiXumutifile/motorfunction.ino
@@ -1,33 +1,8 @@
-/*
-  MotorFunctions.ino - Arduino Sketch for Motor Control
-  
-  Description:
-  This sketch provides functions for controlling a RLSK robot, allowing the
-  motors to move forward and stop.
-
-  Functions:
-  1. void moveForward()
-     - Activates the motor to move forward.
-
-  2. void stopMotor()
-     - Stops the motor.
-
-  Created by: Your Name
-  Date: Current Date
-  Version: 1.0
-*/
-
-/* Moves robot forward: both motors forward same speed */
 void forward() {
     enableMotor(BOTH_MOTORS);
     setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
     setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
     setMotorSpeed(BOTH_MOTORS, fastSpeed);
-}
-
-/* Stops robot forward: both motors disabled */
-void stop() {
-    disableMotor(BOTH_MOTORS);
 }
 
 void backward(){
@@ -41,32 +16,35 @@ void turnleft(){
     enableMotor(BOTH_MOTORS);
     setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
     setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorSpeed(LEFT_MOTOR, lowSpeed);
     setMotorSpeed(RIGHT_MOTOR, fastSpeed);
+    setMotorSpeed(LEFT_MOTOR, lowSpeed);
 }
 
 void turnright(){
     enableMotor(BOTH_MOTORS);
     setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
     setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorSpeed(LEFT_MOTOR, fastSpeed);
     setMotorSpeed(RIGHT_MOTOR, lowSpeed);
+    setMotorSpeed(LEFT_MOTOR, fastSpeed);
 }
 
-
-void spincounterclock(){
+void spincounter(){
     enableMotor(BOTH_MOTORS);
-    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);
-    setMotorSpeed(LEFT_MOTOR, fastSpeed);
+    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_BACKWARD);
+    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
     setMotorSpeed(RIGHT_MOTOR, fastSpeed);
+    setMotorSpeed(LEFT_MOTOR, fastSpeed);
 }
 
 void spinclock(){
     enableMotor(BOTH_MOTORS);
-    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_BACKWARD);
-    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorSpeed(LEFT_MOTOR, fastSpeed);
+    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
+    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);
     setMotorSpeed(RIGHT_MOTOR, fastSpeed);
+    setMotorSpeed(LEFT_MOTOR, fastSpeed);
 }
 
+/* Stops robot forward: both motors disabled */
+void stop() {
+    disableMotor(BOTH_MOTORS);
+}
